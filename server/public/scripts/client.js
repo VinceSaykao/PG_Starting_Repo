d59f0f8a3eb12794e5d2913283c559d566e6cf6a
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-02-10 09:24:57 -0600

    inputs are purple

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,24 +3,57 @@ $(document).ready(onReady);
 function onReady() {
     getSongs();
     $('#add').on('click', postSong);
-}
+    $('#songsTableBody').on('click', '.btn-delete', deleteSong);
+    $('#songsTableBody').on('click','.btn-vote', voteSong);
+};
 
+function voteSong() {
+    // call ajax for PUT
+    // need id and direction
+    let id = $(this).closest('tr').data().id
+    let direction = $(this).text().toLowerCase();
+    console.log(id, direction);
+
+$.ajax({
+    method: 'PUT',
+    url: `/songs/${id}`,
+    data: {
+        direction: direction
+    }
+}).then(function(response) {
+    getSongs();
+}).catch(function(err){
+    console.log(err);
+})
+}; //end of function
+
+
+//brute force
 // get artist data from the server
 function getSongs() {
     $("#songsTableBody").empty();
+    // let songId = 90;
     $.ajax({
         type: 'GET',
-        url: '/songs'
+        // url: `/songs/${songId}`
+        url: `/songs`
     }).then(function (response) {
         console.log("GET /songs response", response);
         // append data to the DOM
         for (let i = 0; i < response.length; i++) {
             $('#songsTableBody').append(`
-                <tr>
+                <tr data-id=${response[i].id}>
                     <td>${response[i].artist}</td>
                     <td>${response[i].track}</td>
-                    <td>${response[i].rank}</td>
+                    <td>${response[i].rank}
+                    <button class="btn-vote">Up</button>
+                    <button class="btn-vote">Down</button>
+                    </td>
                     <td>${response[i].published}</td>
+                    <td>
+                    <button class="btn-delete" data-id=${response[i].id}>DELETE</button>
+                    </td>
+                    
                 </tr>
             `);
         }
@@ -46,3 +79,24 @@ function postSong() {
         getSongs();
     });
 }
+
+// delete is a http method
+// use input parameters if not hard coded in server and if youre using in that instance on page load
+function deleteSong() {
+    let songId = $(this).data().id; // grabs the this id and parse it into our url
+    $.ajax({
+        type: 'DELETE', // type or method same thing
+        url: `/songs/${songId}`
+    })
+    .then(function(response) {
+        console.log('Deleted it!', response);
+        getSongs();
+    })
+    .catch(function(error) {
+        console.log('Error DELETEing', error);
+    })
+} // end of function
+
+
+
+
